03e5866ca6e9e2eb681bebecb60a55302267a973
Caio Mota <caiocvlmota@gmail.com>
2024-08-31 21:09:28 -0300

    finished up the code

--- Assignment 21/sketch.js
+++ Assignment 21/sketch.js
@@ -1,119 +1,105 @@
-// Stores the images for the character
-var oil1, oil2, oil3, oil4;
+// variables to store the images
+var oil1, oil2, oil3, oil4, bg;
 
-// Constants for the different states of the character
-const LEFT = 0;
-const MIDDLE_LEFT = 1;
-const MIDDLE_RIGHT = 2;
-const RIGHT = 3;
-const MIDDLE_RIGHT_BACK = 4;
-const MIDDLE_LEFT_BACK = 5;
+// constants to represent the character states
+const CHAR_LEFT = 0; // state for the character to be facing left
+const CHAR_MIDDLE_LEFT = 1; // state for the character to be in the middle, coming from the left
+const CHAR_MIDDLE_RIGHT = 2; // state for the character to be in the middle, going to the right
+const CHAR_RIGHT = 3; // state for the character to be facing right
+const CHAR_MIDDLE_RIGHT_BACK = 4; // state for the character to be in the middle, coming from the right, but going backwards
+const CHAR_MIDDLE_LEFT_BACK = 5; // state for the character to be in the middle, going to the left, but going backwards
 
-// Stores the current time in milliseconds, the animation time in milliseconds, and the state time in milliseconds
+// variables to control the animation time
 let ms, animTime, stateTime;
 
-// Stores the current state of the character
-let charState = LEFT;
+// variable to control the current character state
+let charState = CHAR_RIGHT;
 
-/**
- * Loads the images for the character.
- */
+// function to load the images
 function preload() {
-  oil1 = loadImage("oil1.png");
-  oil2 = loadImage("oil2.png");
-  oil3 = loadImage("oil3.png");
-  oil4 = loadImage("oil4.png");
+  oil1 = loadImage("oil1.png"); // image of the character facing left
+  oil2 = loadImage("oil2.png"); // image of the character in the middle left
+  oil3 = loadImage("oil3.png"); // image of the character in the middle right
+  oil4 = loadImage("oil4.png"); // image of the character facing right
+  bg = loadImage("bg.png"); // background image
 }
 
-/**
- * Sets up the canvas and initializes the variables.
- */
+// function to create the canvas and set the animation time
 function setup() {
-  // Create a canvas with a size of 32x24
-  createCanvas(32, 24);
-  // Set the animation time to 2 seconds
-  animTime = 2000;
-  // Set the state time to 0
-  stateTime = 0;
+  createCanvas(160, 144);
+  animTime = 700; // animation time in milliseconds
+  stateTime = 0; // current state time in milliseconds
 }
 
-/**
- * Main draw loop of the program. This is where the canvas is redrawn
- * and the character animation is updated.
- */
+// function to draw the character and the background
 function draw() {
-  // Clear the canvas by setting the background color to white
-  background(255);
-  // Update the character animation state
-  MEF();
-  // Draw the character image based on the current state
-  drawChar();
+  image(bg, 0, 0); // draw the background
+  MEF(); // change the character state based on the time
+  drawChar(); // draw the character based on the current state
 }
 
-/**
- * Switches the character state every 2 seconds.
- */
+// function to change the character state based on the time
 function MEF() {
-  // Get the current time in milliseconds
-  ms = millis();
-  // If the current time is greater than the animation time
-  // plus the state time, switch the character state
-  if (ms > animTime + stateTime) {
-    // Switch the character state
-    switch (charState) {
-      case LEFT:
-        // Move to the left
-        charState = MIDDLE_LEFT;
-        break;
-      case MIDDLE_LEFT:
-        // Move to the middle left
-        charState = MIDDLE_RIGHT;
-        break;
-      case MIDDLE_RIGHT:
-        // Move to the middle right
-        charState = RIGHT;
-        break;
-      case RIGHT:
-        // Move to the right
-        charState = MIDDLE_RIGHT_BACK;
-        break;
-      case MIDDLE_RIGHT_BACK:
-        // Move to the middle right back
-        charState = MIDDLE_LEFT_BACK;
-        break;
-      case MIDDLE_LEFT_BACK:
-        // Move to the middle left back
-        charState = LEFT;
-        break;
-    }
-    // Set the state time to the current time
-    stateTime = ms;
+  ms = millis(); // current time in milliseconds
+
+  // change the character state based on the time
+  switch (charState) {
+    case CHAR_LEFT:
+      if (ms > 2 * animTime + stateTime) {
+        charState = CHAR_MIDDLE_LEFT;
+        stateTime = ms;
+      }
+      break;
+    case CHAR_MIDDLE_LEFT:
+      if (ms > animTime + stateTime) {
+        charState = CHAR_MIDDLE_RIGHT;
+        stateTime = ms;
+      }
+      break;
+    case CHAR_MIDDLE_RIGHT:
+      if (ms > animTime + stateTime) {
+        charState = CHAR_RIGHT;
+        stateTime = ms;
+      }
+      break;
+    case CHAR_RIGHT:
+      if (ms > 2 * animTime + stateTime) {
+        charState = CHAR_MIDDLE_RIGHT_BACK;
+        stateTime = ms;
+      }
+      break;
+    case CHAR_MIDDLE_RIGHT_BACK:
+      if (ms > animTime + stateTime) {
+        charState = CHAR_MIDDLE_LEFT_BACK;
+        stateTime = ms;
+      }
+      break;
+    case CHAR_MIDDLE_LEFT_BACK:
+      if (ms > animTime + stateTime) {
+        charState = CHAR_LEFT;
+        stateTime = ms;
+      }
+      break;
   }
 }
 
-/**
- * Draws the character depending on the current state.
- */
+// function to draw the character based on the current state
 function drawChar() {
-  // Switch the character state
   switch (charState) {
-    case LEFT:
-      // Draw the left character
-      image(oil1, 2, 2);
+    case CHAR_LEFT:
+      image(oil1, 28, 90); // draw the character facing left
       break;
-    case MIDDLE_LEFT:
-    case MIDDLE_LEFT_BACK:
-      // Draw the middle left character
-      image(oil2, 2, 2);
+    case CHAR_MIDDLE_LEFT:
+    case CHAR_MIDDLE_LEFT_BACK:
+      image(oil2, 54, 90); // draw the character in the middle left
       break;
-    case MIDDLE_RIGHT:
-    case MIDDLE_RIGHT_BACK:
-      // Draw the middle right character
-      image(oil3, 2, 2);
+    case CHAR_MIDDLE_RIGHT:
+    case CHAR_MIDDLE_RIGHT_BACK:
+      image(oil3, 84, 90); // draw the character in the middle right
       break;
-    case RIGHT:
-      // Draw the right character
-      image(oil4, 2, 2);
+    case CHAR_RIGHT:
+      image(oil4, 107, 90); // draw the character facing right
       break;
   }
 }
+
